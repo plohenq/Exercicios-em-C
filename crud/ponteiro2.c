df1df01f4af699222800f6c6e7806ae1df69e1a8
Paulo Henrique <paulohsa99@gmail.com>
2024-10-23 17:37:21 -0300

    multiplos pontos

--- crud/ponteiro2.c
+++ crud/ponteiro2.c
@@ -3,7 +3,7 @@
 struct Ponto 
 {
     float x;
-    float y;z
+    float y;
 };
 
 void deslocarPonto (struct Ponto *p, float dx, float dy)
@@ -14,26 +14,49 @@ void deslocarPonto (struct Ponto *p, float dx, float dy)
 
 int main (void)
 {
-    struct Ponto ponto1;
+    struct Ponto ponto1; // ponto 1 usando a estrutura
+    struct Ponto ponto2; // ponto 2 usando a estrutura
 
-    printf("Coordenada x: ");
+    // Coordenadas ponto 1
+    printf("Coordenada x do ponto 1: ");
     scanf("%f", &ponto1.x);
 
-    printf("Coordenada y: ");
+    printf("Coordenada y do ponto 1: ");
     scanf("%f", &ponto1.y);
 
-    printf("Coordenadas Atuais: (x: %.2f, y: %.2f)\n", ponto1.x, ponto1.y);
+    printf("Coordenadas Atuais do ponto 1: (x: %.2f, y: %.2f)\n", ponto1.x, ponto1.y);
 
-    float dx, dy;
-    printf("Deslocamento x: ");
-    scanf("%f", &dx);
+    // Coordenadas ponto 2
+    printf("Coordenada x do ponto 2: ");
+    scanf("%f", &ponto2.x);
 
-    printf("Deslocamento y: ");
-    scanf("%f", &dy);
+    printf("Coordenada y do ponto 2: ");
+    scanf("%f", &ponto2.y);
 
-    deslocarPonto(&ponto1, dx, dy);
+    printf("Coordenadas Atuais do ponto 2: (x: %.2f, y: %.2f)\n", ponto2.x, ponto2.y);
 
-    printf("Novas Coordenadas: (x: %.2f, y: %.2f)", ponto1.x, ponto1.y);
+    // Deslocamento ponto 1
+    float dx1, dy1, dx2, dy2;
+    printf("Deslocamento x do ponto 1: ");
+    scanf("%f", &dx1);
+
+    printf("Deslocamento y do ponto 1: ");
+    scanf("%f", &dy1);
+
+    deslocarPonto(&ponto1, dx1, dy1);
+
+    printf("Novas coordenadas do ponto 1: (x: %.2f, y: %.2f)\n", ponto1.x, ponto1.y);
+
+    // Deslocamento ponto 2
+    printf("Deslocamento x do ponto 2: ");
+    scanf("%f", &dx2);
+
+    printf("Deslocamento y do ponto 2: ");
+    scanf("%f", &dy2);
+
+    deslocarPonto(&ponto2, dx2, dy2);
+
+    printf("Novas coordenadas do ponto 2: (x: %.2f, y: %.2f)", ponto2.x, ponto2.y);
 
     return 0;
 }
